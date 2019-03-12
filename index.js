2657439e6e77a8a680c9e8434d760f6c27914969
sos1819apc <albpercar@gmail.com>
2019-03-12 23:46:59 +0000

    html

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-
 var express = require("express");
 
-var app= express();
-
-var tiempo= new Date();
-var hora= tiempo.getHours();
-var minutos= tiempo.getMinutes();
-var segundos = tiempo.getSeconds();
-
-var horaEntera = hora + ":" + minutos + ":" + segundos;
+var app = express();
 
 var port = process.env.PORT || 8080;
 
-console.log("Hora actual:" , horaEntera);
+app.use("/",express.static(__dirname+"/public"));
 
-app.get("/time", (request, response) => {
-    response.send(Date());
-    console.log("Nueva solicitud recibida");
-});
+app.get("/time",(request,response)=>{
+    response.send(new Date());
+})
 
-app.listen(port, () => {
-    console.log("Server operativo!")
-});
+app.listen(port,() => {
+    
+    console.log("magic is happening in port"+port);
+})
