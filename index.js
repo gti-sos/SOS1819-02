17d91e81ae1ffb33b6594fa02192d127c19dd439
sos1819apc <albpercar@gmail.com>
2019-03-18 17:20:12 +0000

    index.js

--- index.js
+++ index.js
@@ -159,6 +159,167 @@ app.put("/api/v1/movies-stats", (req, res) => {
 });
 
 
+//Recursos Alberto Pérez
+
+var scorersstats = [];
+
+// GET /api/v1/scorers-stats/loadInitialData
+
+app.get("/api/v1/scorers-stats/loadInitialData", (req,res)=>{
+    
+    scorersstats =  [{
+    country: "argentina",
+    year: "2004",
+    name : "lionel-messi",
+    scorergoal : "405",
+    scorermatch: "440",
+    scoreraverage : "0.92"
+}, {
+    country: "portugal",
+    year: "2009",
+    name : "cristiano-ronaldo",
+    scorergoal : "311",
+    scorermatch: "292",
+    scoreraverage : "1.07"
+}];
+
+    res.sendStatus(200);
+});
+
+
+// GET /api/v1/scorers-stats
+
+app.get("/api/v1/scorers-stats", (req,res)=>{
+    res.send(scorersstats);
+});
+
+
+// POST /api/v1/scorers-stats
+
+app.post("/api/v1/scorers-stats", (req,res)=>{
+    
+    var newscorersstats = req.body;
+    
+    scorersstats.push(newscorersstats);
+    
+    res.sendStatus(201);
+});
+
+
+// DELETE /api/v1/scorers-stats
+
+app.delete("/api/v1/scorers-stats", (req,res)=>{
+    
+    scorersstats =  [];
+
+    res.sendStatus(200);
+});
+
+
+// GET /api/v1/scorers-stats/argentina
+
+app.get("/api/v1/scorers-stats/:country", (req,res)=>{
+
+    var country = req.params.country;
+
+    var filteredscorersstats = scorersstats.filter((c) =>{
+       return c.country == country; 
+    });
+    
+    if (filteredscorersstats.length >= 1){
+        res.send(filteredscorersstats);
+    }else{
+        res.sendStatus(404);
+    }
+
+});
+
+
+// PUT /api/v1/scorers-stats/argentina
+
+app.put("/api/v1/scorers-stats/:country", (req,res)=>{
+
+    var country = req.params.country;
+    var updatedscorersstats = req.body;
+    var found = false;
+
+    var updatedscorersstats2 = scorersstats.map((c) =>{
+    
+        if(c.country == country){
+            found = true;
+            return updatedscorersstats;
+        }else{
+            return c;            
+        }
+ 
+    });
+    
+    if (found == false){
+        res.sendStatus(404);
+    }else{
+        scorersstats = updatedscorersstats2;
+        res.sendStatus(200);
+    }
+
+});
+
+
+// DELETE /api/v1/scorers-stats/argentina
+
+app.delete("/api/v1/scorers-stats/:country", (req,res)=>{
+
+    var country = req.params.country;
+    var found = false;
+
+    var updatedCountry = scorersstats.filter((c) =>{
+        
+            if(c.country == country)  
+                found = true;
+        
+            return c.country != country;
+    });
+    
+    if (found == false){
+        res.sendStatus(404);
+    }else{
+        scorersstats = updatedCountry;
+        res.sendStatus(200);
+    }
+
+});
+
+// POST /api/v1/scorers-stats/argentina
+
+app.post("/api/v1/scorers-stats/:country", (req,res)=>{
+
+    res.sendStatus(409);
+});
+
+// PUT /api/v1/scorers-stats
+
+app.put("/api/v1/scorers-stats", (req,res)=>{
+
+    res.sendStatus(409);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
