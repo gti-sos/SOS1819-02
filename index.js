f55280f87c76b4af471f6f4c47f0ba23f665bc53
sos1819pgm <pabgarmar95@hotmail.com>
2019-04-01 22:40:38 +0000

    apis

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("declaracion de variables express y bodyParser");
 var express = require("express");
 var bodyParser = require("body-parser");
 var scorersApi = require("./api-scorers/");
-
+var companiesApi = require("./api-companies/");
 
 
 /___________Paginacion y autentificacion(proceso)_________________/
@@ -36,15 +36,6 @@ client.connect(error => {
     console.log("Connected to database.");
 });
 
-
-//CONECTAR A LA BASEDEDATOS MONGO PABLO
-const clientpgm = new MongoClient(pgm, { useNewUrlParser: true });
-
-clientpgm.connect(error => {
-    companies = clientpgm.db("sos1819-02-pgm").collection("companies");
-
-    console.log("Connected to database de Pablo.");
-});
 
 console.log("conectadas las 3 bases de datos");
 
@@ -284,13 +275,6 @@ console.log("GET a docs");
 app.get("/api/v1/companies-stats/docs", (req, res) => {
     res.redirect("https://documenter.getpostman.com/view/6990295/S17oyqep");
 })
-console.log("Declara companiesstats vacia");
-var companiesstats = [];
-
-
-// GET /api/v1/companies-stats/loadInitialData
-console.log("GET loadInitialData");
-app.get("/api/v1/companies-stats/loadInitialData", (req, res) => {
 
     var companiesstatsinitial = [{
             country: "EEUU",
@@ -334,156 +318,8 @@ app.get("/api/v1/companies-stats/loadInitialData", (req, res) => {
             income: "169,7",
             marketcapitalization: "369.57",
             employee: "307000"
-        }
-
-
-    ];
-    console.log("cargadas companiesstatsinitial");
-    companies.find({}).toArray((error, companiesArray) => {
-        if (companiesArray.length == 0) {
-            companies.insert(companiesstatsinitial);
-            res.sendStatus(200);
-        }
-        else {
-            res.sendStatus(409);
-        }
-    });
-
-});
-
-
-// GET /api/v1/companies-stats
-console.log("GET al conjunto /companies-stats ");
-app.get("/api/v1/companies-stats", (req, res) => {
-    companies.find({}).toArray((error, companiesArray) => {
-        res.send(companiesArray);
-        if (error) {
-            console.log("Error: " + error);
-        }
-    });
-});
-
-
-// POST /api/v1/companies-stats
-console.log("POST al conjunto /companies-stats ");
-app.post("/api/v1/companies-stats", (req, res) => {
-
-    var newcompaniesstats = req.body;
-    var yearCompany = req.body.year;
-    var newCompanies = req.body;
-
-    if (!newCompanies.country || !newCompanies.year || !newCompanies.company || !newCompanies.income || !newCompanies.marketcapitalization || !newCompanies.employee) {
-        res.sendStatus(400);
-    }
-    companies.find({ "year": yearCompany }).toArray((error, companiesArray) => {
-
-        if (error) {
-            console.log("Error: " + error);
-        }
-        if (companiesArray.length > 0) {
-            res.sendStatus(409);
-        }
-        else {
-            companies.insert(newcompaniesstats);
-            res.sendStatus(201);
-        }
-    });
-});
-
-
-// DELETE /api/v1/companies-stats
-console.log("DELETE al conjunto /companies-stats ");
-app.delete("/api/v1/companies-stats", (req, res) => {
-
-    companies.remove({});
-    //console.log("Request accepted, removing all resources of database.");
-    res.sendStatus(200);
-
-});
-
-
-// GET /api/v1/companies-stats/2007
-console.log("GET al AÑO /companies-stats/2007 ");
-app.get("/api/v1/companies-stats/:year", (req, res) => {
-
-    var year = req.params.year;
-
-    companies.find({ "year": year }).toArray((error, filteredcompaniesstats) => {
-        if (error) {
-            console.log("Error: " + error);
-        }
-        if (filteredcompaniesstats.length >= 1) {
-            res.send(filteredcompaniesstats[0]);
-        }
-        else {
-            res.sendStatus(404);
-        }
-    });
-
-});
-
-
-
-
-// PUT /api/v1/companies-stats/2007
-console.log("GET al año /companies-stats/2007 ");
-app.put("/api/v1/companies-stats/:year", (req, res) => {
-    var id = req.params._id;
-    var year = req.params.year;
-    var updatedcompaniesstats = req.body;
-
-    companies.find({}).toArray((error, companiesArray) => {
-        if (error) {
-            console.log("Error: " + error);
-        }
-        if (year != updatedcompaniesstats.year || id != updatedcompaniesstats._id) {
-            res.sendStatus(400);
-        }
-        else {
-            companies.updateOne({ "year": year }, { $set: updatedcompaniesstats });
-            res.sendStatus(200);
-        }
-
-    });
-
-});
-
-
-// DELETE /api/v1/companies-stats/1997
-console.log("DELETE al AÑO /companies-stats/1997 ");
-app.delete("/api/v1/companies-stats/:year", (req, res) => {
-
-    var year = req.params.year;
-
-    companies.find({ "year": year }).toArray((error, filteredcompaniesstats) => {
-        if (error) {
-            console.log("Error: " + error);
-        }
-        if (filteredcompaniesstats.length === 0) {
-            res.sendStatus(404);
-        }
-        else {
-            companies.deleteOne({ "year": year });
-            res.sendStatus(200);
-        }
-    });
-
-});
-
-// POST /api/v1/companies-stats/1997
-console.log("POST Erroneo al año /companies-stats/1997-->405 ");
-app.post("/api/v1/companies-stats/:year", (req, res) => {
-
-    res.sendStatus(405);
-});
-
-// PUT /api/v1/companies-stats
-console.log("PUT Erroneo al conjunto /companies-stats/1997-->405 ");
-app.put("/api/v1/companies-stats", (req, res) => {
-
-    res.sendStatus(405);
-});
-
+        } ];
+  
 var scorersstatsinitial = [{
         country: "arg",
         year: "2004",
@@ -530,6 +366,16 @@ clientapc.connect(error => {
     console.log("Connected to database.");
 
 });
+
+//CONECTAR A LA BASEDEDATOS MONGO PABLO
+const clientpgm = new MongoClient(pgm, { useNewUrlParser: true });
+
+clientpgm.connect(error => {
+    companies = clientpgm.db("sos1819-02-pgm").collection("companies");
+    companiesApi.register(app,companies,companiesstatsinitial);
+    console.log("Connected to database de Pablo.");
+});
+
 
 
 console.log("GET /time ");
