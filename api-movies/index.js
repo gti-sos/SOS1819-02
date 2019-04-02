a25fbbfbf03b33dff9fcb8dcae031b7b9c7a9fe9
sos1819amm <alemarman1@gmail.com>
2019-04-02 15:38:29 +0000

    heroku

--- api-movies/index.js
+++ api-movies/index.js
@@ -190,7 +190,6 @@ moviesApi.register = function(app, movies, moviesstatsinitial) {
     });
 
 
-
     // POST /api/v1/movies-stats/EEUU/1997
     console.log("POST erroneo al año movies-stats/EEUU/1997--> 405");
     app.post(BASE_PATH + "/movies-stats/:country/:year", (req, res) => {
