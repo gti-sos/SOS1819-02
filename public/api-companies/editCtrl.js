d673c5b2b21c90b50698db2bf8286fcd1030bc4f
sos1819pgm <pabgarmar95@hotmail.com>
2019-04-22 10:13:40 +0000

    f07

---
+++ public/api-companies/editCtrl.js
@@ -0,0 +1,29 @@
+
+/* global angular $scope*/
+
+var app = angular.module("CompaniesApp");
+
+
+app.controller("editCtrl", ["$scope","$http","$routeParams", "$location", function ($scope, $http,$routeParams,$location){
+                    console.log("Companies editCtrl Initialized!");
+                    var API = "/api-companies/v1/compnaies-stats/"+$routeParams.country+"/"+$routeParams.year;
+                    
+                    $http.get(API).then(function(response){
+                        console.log("Datos recibidos: "+ JSON.stringify(response.data,null,2));
+                        $scope.updatedCompany = response.data;
+                    }, function (error){
+                        $scope.status = error.status;
+                        $scope.data = "";
+                        });
+                    
+                    
+                    
+                    $scope.updateCompany = function(){
+                         $http.put(API,$scope.updatedCompany).then(function(response){
+                         $location.path("/");
+                    }, function (error){
+                        $scope.status = error.status;
+                        $scope.data = "";
+                        });
+                    }; 
+}]);
