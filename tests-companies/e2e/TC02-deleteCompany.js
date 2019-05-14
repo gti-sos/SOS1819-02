faee912660d417b16ac42c07237e73d54146381d
sos1819pgm <pabgarmar95@hotmail.com>
2019-05-14 19:57:56 +0000

    D02

--- tests-companies/e2e/TC02-deleteCompany.js
+++ tests-companies/e2e/TC02-deleteCompany.js
@@ -1,18 +1,18 @@
-describe('Checking if an Tourist is deleted', function() {
-    it('should show some Tourist', function() {
+describe('Checking if an compay is deleted', function() {
+    it('should show some companies', function() {
         browser
-            .get('https://sos1819-02.herokuapp.com/#!/api-companies/');
+            .get('https://sos1819-02.herokuapp.com/#!/api-companies');
 
         element
             .all(by.repeater("company in companies"))
             .then(function(initialCompanies) {
                 console.log(initialComapnies.length);
                 browser.driver.sleep(2000);
-                element.all(by.css('[value="Borrar"]')).last().click();
+                element.all(by.css('[value="Borrar"]')).click();
 
                 element.all(by.repeater("company in companies"))
                     .then(function(finalCompanies) {
-                        expect(finalCompanies.length).toEqual(initialCompanies.length-1);
+                        expect(finalCompanies.length).toEqual(0);
 
                     });
             });
