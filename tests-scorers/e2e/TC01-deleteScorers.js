301acc04ba63abeded2809f1bc8e6eaba063ae4c
sos1819apc <albpercar@gmail.com>
2019-05-14 18:14:30 +0000

    a

--- tests-scorers/e2e/TC01-deleteScorers.js
+++ tests-scorers/e2e/TC01-deleteScorers.js
@@ -1,21 +1,42 @@
-describe('Checking if an expense is deleted', function(){
-    it('should show some expenses',function(){
-        browser
-            .get("https://sos1819-02.herokuapp.com/#!/api-scorers");
+describe("Check if a new scorer can be created: ", function(){
+        it("List should grow after the Scorer creation", function(){
             
+                browser.get("https://sos1819-02.herokuapp.com/#!/api-scorers");
+                
+                check();
+                
+                function check(){
+                   
+                    element
+                        .all(by.repeater("scorer in scorers"))
+                        .then(function (initialScorers){
+                                
+                                if(initialScorers.length==10){
+                                    
+                                    element(by.css('[value="Siguiente Página"]')).click().then(check());
+                                    
+                                } else {
 
-         element
-                .all(by.repeater("scorer in scorers"))
-                .then(function(initialScorers){
-                     console.log(initialScorers.length);
-                      browser.driver.sleep(2000);
-                      element.all(by.css('[value="delete"]')).last().click();
-                      
-                      element.all(by.repeater("scorer in scorers"))
-                .then(function(finalScorers){
-                 expect(finalScorers.length).toEqual(initialScorers.length);
-                    
-                    
-                    
-                })
-                })})})
+                                    element(by.css('[value="delete"]')).click();
+                                    
+                                    element
+                                        .all(by.repeater("scorer in scorers"))
+                                        .then(function(finalScorers){
+                                            
+                                                expect(finalScorers.length).toEqual(initialScorers.length-1);
+                                            
+                                            }
+                                        );
+                                        
+                                }
+                                
+                            }
+                        );
+                        
+                }
+                
+            }
+        );
+    
+    }
+);
