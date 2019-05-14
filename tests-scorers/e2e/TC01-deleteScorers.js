de866f1e87cd2453f2452d012001451466e2cc58
sos1819apc <albpercar@gmail.com>
2019-05-14 17:14:32 +0000

    a

--- tests-scorers/e2e/TC01-deleteScorers.js
+++ tests-scorers/e2e/TC01-deleteScorers.js
@@ -1,14 +1,14 @@
 describe('Checking if an scorer is deleted', function(){
     it('should show some scorer',function(){
         browser
-            .get("https://sos1819-02.herokuapp.com/#!/api-scorers/country123/123");
+            .get("https://sos1819-02.herokuapp.com/#!/api-scorers/");
             
              element
                 .all(by.repeater("scorer in scorers"))
                 .then(function(initialScorers){
                      console.log("esto es el initial Scorers"+initialScorers.length);
                       browser.driver.sleep(2000);
-                      element.all(by.css('[value="delete"]')).last().click();
+                      element.all(by.css('[value="delete"]')).click();
                       
                       element.all(by.repeater("scorer in scorers"))
                 .then(function(finalScorers){
