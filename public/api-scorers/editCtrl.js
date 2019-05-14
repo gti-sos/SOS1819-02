f53c85329c904ff6e4d763fa48c16cece5e28ab2
sos1819apc <albpercar@gmail.com>
2019-05-14 22:21:55 +0000

    a

--- public/api-scorers/editCtrl.js
+++ public/api-scorers/editCtrl.js
@@ -4,7 +4,7 @@ var app = angular.module("AppManager");
 
 app.controller("editCtrl", ["$scope","$http","$routeParams", "$location", function ($scope, $http,$routeParams,$location){
                     console.log("Scorers editCtrl Initialized!");
-                    var API = "/!#/api-scorers"+$routeParams.country+"/"+$routeParams.year;
+                    var API = "/api/v1/scorers-stats/"+$routeParams.country+"/"+$routeParams.year;
                     
                     $http.get(API).then(function(response){
                         console.log("Datos recibidos: "+ JSON.stringify(response.data,null,2));
