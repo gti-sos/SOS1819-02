127f24fa2238b7b089225a8779bedf59991bd204
sos1819pgm <pabgarmar95@hotmail.com>
2019-05-14 19:34:24 +0000

    D02

--- tests-companies/e2e/TC02-deleteCompany.js
+++ tests-companies/e2e/TC02-deleteCompany.js
@@ -1,42 +1,20 @@
-describe('Checking if an expense is deleted', function() {
-    it('should show some expenses', function() {
+describe('Checking if an Tourist is deleted', function() {
+    it('should show some Tourist', function() {
         browser
-            .get("https://sos1819-02.herokuapp.com/#!/api-companies");
-        check();
+            .get('https://sos1819-02.herokuapp.com/#!/api-companies/');
 
-        function check() {
+        element
+            .all(by.repeater("company in companies"))
+            .then(function(initialCompanies) {
+                console.log(initialComapnies.length);
+                browser.driver.sleep(2000);
+                element.all(by.css('[value="Borrar"]')).last().click();
 
+                element.all(by.repeater("company in companies"))
+                    .then(function(finalCompanies) {
+                        expect(finalCompanies.length).toEqual(initialCompanies.length-1);
 
-            element
-                .all(by.repeater("company in companies"))
-                .then(function(initialCompanies) {
-                    if (initialCompanies.length < 10) {
-
-
-                        console.log(initialCompanies.length);
-
-                        browser.driver.sleep(2000);
-
-                        element.all(by.css('[value="Borrar Country123/123"]')).click();
-
-                        element.all(by.repeater("company in companies"))
-                            .then(function(finalCompanies) {
-                                expect(initialCompanies.length).toBeGreaterThan(finalCompanies.length);
-                            });
-                    }
-                    else if (initialCompanies.length == 0) {
-                        element(by.css('[value="Página Anterior"]')).click();
-                        element(by.css('[value="Borrar Country123/123"]')).click();
-                        element.all(by.repeater("company in companies"))
-                            .then(function(finalCompanies) {
-                               // expect(finalCompanies.length).toEqual(initialCompanies.length - 1);
-                                expect(initialCompanies.length).toBeGreaterThan(finalCompanies.length);
-                            });
-                    }
-                    else {
-                        element(by.css('value="Siguiente Página"]')).click().then(check());
-                    }
-                });
-        }
+                    });
+            });
     });
 });
