f703cb8e6ce14527ddf5269eba62177d9c98aaa1
sos1819pgm <pabgarmar95@hotmail.com>
2019-05-14 18:39:56 +0000

    D02

--- tests-companies/tests-companies.js
+++ tests-companies/tests-companies.js
@@ -3,9 +3,9 @@ exports.config = {
     seleniumAddress: "http://localhost:4444/wd/hub",
     chromeOnly: true,
     specs: [
-        "e2e/TC01-loadCompanies.js",
+        "e2e/TC00-loadCompanies.js",
         "e2e/TC01-createCompany.js",
-        "e2e/TC01-deleteCompany.js"
+        "e2e/TC02-deleteCompany.js"
     ]
 
 };
