6d2ddb49b994baa6b24213560e4341a49ddbdb83
sos1819amm <alemarman1@gmail.com>
2019-05-14 10:33:24 +0000

    D02

--- tests-movies/e2e/TM01-deleteMovie.js
+++ tests-movies/e2e/TM01-deleteMovie.js
@@ -1,59 +1,20 @@
-describe("Check if a movie can be deleted: ", function(){
-    
-        it("List should be wane after the movie deletion.", function(){
-                
-                browser.get("https://sos1819-02.herokuapp.com/#!/api-movies");
-                
-                check();
-                
-                function check(){
+describe('Checking if an expense is deleted', function(){
+    it('should show some expenses',function(){
+        browser
+            .get("https://sos1819-02.herokuapp.com/#!/api-movies");
+            
+             element
+                .all(by.repeater("movie in movies"))
+                .then(function(initialMovies){
+                     console.log(initialMovies.length);
+                      browser.driver.sleep(2000);
+                      element.all(by.css('[value="Borrar"]')).last().click();
+                      
+                      element.all(by.repeater("movie in movies"))
+                .then(function(finalMovies){
+                 expect(finalMovies.length).toEqual(initialMovies.length-1);
                     
-                    element
-                        .all(by.repeater("movie in movies"))
-                        .then(function(initialMovies){
-                                
-                                if(initialMovies.length<10){
-                                    
-                                 element.all(by.css('[value="Borrar"]')).last().click();
-
-                                    
-                                    element
-                                        .all(by.repeater("movie in movies"))
-                                        .then(function(finalMovies){
-                                                
-                                        expect(finalMovies.length).toEqual(initialMovies.length-1);                                                
-                                            }
-                                        );
-                                    
-                                } else if(initialMovies.length==0){
-                                    
-                                    element(by.css('[value="Página Anterior"]')).click();
-                                    
-                                    element.all(by.css('[value="Borrar"]')).last().click();
-                                    
-                                    element
-                                        .all(by.repeater("movie in movies"))
-                                        .then(function(finalMovies){
-                                                
-                                                expect(initialMovies.length).toBeGreaterThan(finalMovies.length);
-                                                
-                                            }
-                                        );
-                                    
-                                } else {
-                                    
-                                    element(by.css('[value="Siguiente Página"]')).click().then(check());
-                                    
-                                }
-                                
-                            }
-                        );
                     
-                }
-                
-                
-            }
-        );
-    
-    }
-);
+                    
+                })
+                })})})
