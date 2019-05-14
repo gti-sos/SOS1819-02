01f777d11d513ba97b8cae5e3c7cdf5f9edb7a03
sos1819apc <albpercar@gmail.com>
2019-05-14 18:41:18 +0000

    a

--- tests-scorers/e2e/TC01-deleteScorers.js
+++ tests-scorers/e2e/TC01-deleteScorers.js
@@ -1,23 +1,59 @@
-describe('Checking if an scorer is deleted', function(){
-    it('should show some scorers',function(){
-        browser
-            .get("https://sos1819-02.herokuapp.com/#!/api-scorers");
-            
-
-         element
-                .all(by.repeater("scorer in scorers"))
-                console.log(scorer.name)
-                    console.log(scorer.country)
-                .then(function(initialScorers){
-                     console.log(initialScorers.length);
-                      browser.driver.sleep(2000);
-                      element.all(by.css('[value="delete"]')).last().click();
-                      
-                      element.all(by.repeater("scorer in scorers"))
-                .then(function(finalScorers){
-                 expect(finalScorers.length).toEqual(initialScorers.length-1);
+describe("Check if a scorer can be deleted: ", function(){
+    
+        it("List should be wane after the scorer deletion.", function(){
+                
+                browser.get("https://sos1819-02.herokuapp.com/#!/api-scorers/");
+                
+                check();
+                
+                function check(){
                     
+                    element
+                        .all(by.repeater("scorer in scorers"))
+                        .then(function(initialScorers){
+                                
+                                if(initialScorers.length<10){
+                                    
+                                    element(by.css('[value="delete"]')).click();
+                                    
+                                    element
+                                        .all(by.repeater("scorer in scorers"))
+                                        .then(function(finalScorers){
+                                                
+                                                expect(initialScorers.length).toBeGreaterThan(finalScorers.length);
+                                                
+                                            }
+                                        );
+                                    
+                                } else if(initialScorer.length==0){
+                                    
+                                    element(by.css('[value="Página Anterior"]')).click();
+                                    
+                                    element(by.css('[value="delete"]')).click();
+                                    
+                                    element
+                                        .all(by.repeater("scorer in scorers"))
+                                        .then(function(finalScorers){
+                                                
+                                                expect(initialScorers.length).toBeGreaterThan(finalScorers.length);
+                                                
+                                            }
+                                        );
+                                    
+                                } else {
+                                    
+                                    element(by.css('[value="Siguiente Página"]')).click().then(check());
+                                    
+                                }
+                                
+                            }
+                        );
                     
-                    
-                })
-                })})})
+                }
+                
+                
+            }
+        );
+    
+    }
+);
