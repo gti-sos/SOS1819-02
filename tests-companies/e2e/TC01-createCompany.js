14b19fd3d7011ef44248b08b29049c4919caeb5d
sos1819pgm <pabgarmar95@hotmail.com>
2019-05-14 20:33:05 +0000

    D02

--- tests-companies/e2e/TC01-createCompany.js
+++ tests-companies/e2e/TC01-createCompany.js
@@ -1,49 +1,26 @@
-describe("Check if a new company can be created: ", function(){
-        it("List should grow after the Company creation", function(){
-            
-                browser.get("https://sos1819-02.herokuapp.com/#!/api-companies");
-                
-                check();
-                
-                function check(){
-                   
-                    element
-                        .all(by.repeater("company in companies"))
-                        .then(function (initialCompanies){
-                                
-                                if(initialCompanies.length==0){
-                                    
-                                    element(by.css('[value="Siguiente Página"]')).click().then(check());
-                                    
-                                } else {
-                                
-                                    element(by.model('newCompany.company')).sendKeys('company123');
-                                    element(by.model('newCompany.country')).sendKeys('Country123');
-                                    element(by.model('newCompany.year')).sendKeys(123);
-                                    element(by.model('newCompany.income')).sendKeys(123);
-                                    element(by.model('newCompany.marketcapitalization')).sendKeys(123);
-                                    element(by.model('newCompany.employee')).sendKeys(123);
+describe("Check if a new company can be created: ", function() {
+    it("List should grow after the Company creation", function() {
 
-                                    element(by.css('[value="Añadir"]')).click();
-                                    
-                                    element
-                                        .all(by.repeater("company in companies"))
-                                        .then(function(finalCompanies){
-                                            
-                                                expect(finalCompanies.length).toEqual(initialCompanies.length+1);
-                                            
-                                            }
-                                        );
-                                        
-                                }
-                                
-                            }
-                        );
-                        
-                }
-                
-            }
-        );
-    
-    }
-);
+        browser.get("https://sos1819-02.herokuapp.com/#!/api-companies");
+        element
+            .all(by.repeater("company in companies"))
+            .then(function(initialCompanies) {
+                element(by.model('newCompany.company')).sendKeys('company123');
+                element(by.model('newCompany.country')).sendKeys('Country123');
+                element(by.model('newCompany.year')).sendKeys(123);
+                element(by.model('newCompany.income')).sendKeys(123);
+                element(by.model('newCompany.marketcapitalization')).sendKeys(123);
+                element(by.model('newCompany.employee')).sendKeys(123);
+
+                element(by.css('[value="Añadir"]')).click();
+
+                element
+                    .all(by.repeater("company in companies"))
+                    .then(function(finalCompanies) {
+
+                        expect(finalCompanies.length).toEqual(initialCompanies.length + 1);
+
+                    });
+            });
+    });
+});
--- tests-companies/e2e/TC02-deleteCompany.js
+++ tests-companies/e2e/TC02-deleteCompany.js
@@ -6,13 +6,11 @@ describe('Checking if an company is deleted', function() {
         element
             .all(by.repeater("company in companies"))
             .then(function(initialCompanies) {
-                console.log(initialCompanies.length);
-                browser.driver.sleep(2000);
-                element.all(by.css('[value="Borrar"]')).click();
+                element(by.css('[value="Borrar"]')).click();
 
                 element.all(by.repeater("company in companies"))
                     .then(function(finalCompanies) {
-                        expect(finalCompanies.length).toEqual(0);
+                        expect(finalCompanies.length).toEqual(initialCompanies.length-1);
 
                     });
             });
