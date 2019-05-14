cfe7e3068ae9cf3f42de7556b219143fd743d3b7
sos1819pgm <pabgarmar95@hotmail.com>
2019-05-14 17:12:20 +0000

    D02

--- tests-companies/e2e/TC01-deleteCompany.js
+++ tests-companies/e2e/TC01-deleteCompany.js
@@ -2,21 +2,40 @@ describe('Checking if an expense is deleted', function() {
     it('should show some expenses', function() {
         browser
             .get("https://sos1819-02.herokuapp.com/#!/api-companies");
+        check();
 
-        element
-            .all(by.repeater("company in companies"))
-            .then(function(initialCompanies) {
-                console.log(initialCompanies.length);
-                browser.driver.sleep(2000);
-                element.all(by.css('[value="Borrar"]')).last().click();
-
-                element.all(by.repeater("company in companies"))
-                    .then(function(finalCompanies) {
-                        expect(finalCompanies.length).toEqual(initialCompanies.length - 1);
+        function check() {
 
 
+            element
+                .all(by.repeater("company in companies"))
+                .then(function(initialCompanies) {
+                    if (initialCompanies.length < 10) {
 
-                    });
-            });
+
+                        console.log(initialCompanies.length);
+
+                        //browser.driver.sleep(2000);
+
+                        element.all(by.css('[value="Borrar Country123/123"]')).click();
+
+                        element.all(by.repeater("company in companies"))
+                            .then(function(finalCompanies) {
+                                expect(finalCompanies.length).toEqual(initialCompanies.length - 1);
+                            });
+                    }
+                    else if (initialCompanies.length == 0) {
+                        element(by.css('[value="Página Anterior"]')).click();
+                        element(by.css('[value="Borrar Country123/123"]')).click();
+                        element.all(by.repeater("company in companies"))
+                            .then(function(finalCompanies) {
+                                expect(finalCompanies.length).toEqual(initialCompanies.length - 1);
+                            });
+                    }
+                    else {
+                        element(by.css('value="Siguiente Página"]')).click().then(check());
+                    }
+                });
+        }
     });
 });
