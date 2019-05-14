ad69e9775542bfc793de5be872c6e5a406abf6f2
sos1819pgm <pabgarmar95@hotmail.com>
2019-05-14 21:20:41 +0000

    D02

--- tests-companies/e2e/TC02-deleteCompany.js
+++ tests-companies/e2e/TC02-deleteCompany.js
@@ -10,7 +10,7 @@ describe('Checking if an company is deleted', function() {
 
                 element.all(by.repeater("company in companies"))
                     .then(function(finalCompanies) {
-                        expect(finalCompanies.length).toEqual(11);
+                        expect(finalCompanies.length).toEqual(10);
 
                     });
             });
