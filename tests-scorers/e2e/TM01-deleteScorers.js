facfb3b1406f2562e0dfd09bbba543ef8be302ba
sos1819apc <albpercar@gmail.com>
2019-05-14 17:45:12 +0000

    a

---
+++ tests-scorers/e2e/TM01-deleteScorers.js
@@ -0,0 +1,21 @@
+describe('Checking if an expense is deleted', function(){
+    it('should show some expenses',function(){
+        browser
+            .get("https://sos1819-02.herokuapp.com/#!/api-scorers");
+            
+             element
+                .all(by.repeater("scorer in scorers"))
+                .then(function(initialScorers){
+                     console.log(initialScorers.length);
+                      browser.driver.sleep(2000);
+                      element.all(by.css('[value="Borrar"]')).last().click();
+                      
+                      element.all(by.repeater("scorer in scorers"))
+                .then(function(finalScorers){
+                 expect(finalScorers.length).toEqual(0);
+                 console.log("ulti"+initialScorers.length);
+                    
+                    
+                    
+                })
+                })})})
