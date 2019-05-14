0de0b1feadd5ef3dd5fe3b7b68d24a3f74135661
sos1819apc <albpercar@gmail.com>
2019-05-14 17:29:36 +0000

    a

--- tests-scorers/e2e/TC01-deleteScorers.js
+++ tests-scorers/e2e/TC01-deleteScorers.js
@@ -1,20 +1,8 @@
 describe('Checking if an scorer is deleted', function(){
-    it('should show some scorer',function(){
-        browser
-            .get("https://sos1819-02.herokuapp.com/#!/api-scorers/");
-            
-             element
-                .all(by.repeater("scorer in scorers"))
-                .then(function(initialScorers){
-                     console.log("esto es el initial Scorers"+initialScorers.length);
-                      browser.driver.sleep(2000);
-                      element.all(by.css('[value="delete"]')).click();
-                      
-                      element.all(by.repeater("scorer in scorers"))
-                .then(function(finalScorers){
-                 expect(finalScorers.length).toEqual(initialScorers.length-1);
-                    console.log("esto es el final Scorers"+finalScorers.length);
-                    
-                    
-                })
-                })})})
+    it("Should delete the created item", function () {
+		element.all(by.repeater("scorer in scorers")).first().all(by.css('[value="delete"]')).last().click();
+		browser.takeScreenshot().then(function (png) {
+        writeScreenShot(png, 'exception.png');
+    });
+    });
+});
