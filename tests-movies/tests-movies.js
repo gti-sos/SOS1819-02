0d8f2e226f84779310663fdabbc1a04f5f959349
sos1819amm <alemarman1@gmail.com>
2019-05-15 07:49:41 +0000

    D02 finish

--- tests-movies/tests-movies.js
+++ tests-movies/tests-movies.js
@@ -3,9 +3,8 @@ exports.config = {
     seleniumAddress: "http://localhost:4444/wd/hub",
     chromeOnly: true,
     specs: [
-                "e2e/TM01-loadMovies.js",
                 "e2e/TM02-createMovie.js",
+                "e2e/TM01-loadMovies.js",
                 "e2e/TM03-deleteMovie.js"
            ]
-    
 };
