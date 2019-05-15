7c86d70af9350c8969840dc81a9bd9446122840a
sos1819apc <albpercar@gmail.com>
2019-05-15 00:19:57 +0000

    a

--- tests-scorers/tests-scorers.js
+++ tests-scorers/tests-scorers.js
@@ -1,6 +1,6 @@
 exports.config = {
     
-    seleniumAddress: "http://localhost:4445/wd/hub",
+    seleniumAddress: "http://localhost:4444/wd/hub",
     chromeOnly: true,
     specs: [    
                 "e2e/TC02-createScorers.js",
