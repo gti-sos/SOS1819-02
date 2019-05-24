18c2323cce61336173fea0f5250d970806df417e
sos1819apc <albpercar@gmail.com>
2019-05-24 19:14:28 +0000

    a

--- public/api-scorers/scorersGraphCtrl.js
+++ public/api-scorers/scorersGraphCtrl.js
@@ -1,75 +1,134 @@
-
 var app = angular.module("AppManager");
 
 
-app.controller("grafCtrl", ["$scope","$http", function ($scope, $http){
-                    console.log("Scorers grafCtrl Initialized!");
-                    
-    Highcharts.chart('container', {
+app.controller("grafCtrl", ["$scope","$http","$routeParams", "$location", function ($scope, $http,$routeParams,$location){
+                    console.log("Scorers editCtrl Initialized!");
+                    var API = "/api/v1/scorers-stats";
+    var countries = [];
+    var years = [];
+    var names = [];
+    var scorergoals = [];
+    var scorermatches = [];
+    var scoreraverages = [];
+    var sumas = [];
+    var CandYsumas = [];
+    var countryandyear = [];
+    var CandProperties = [];
+    
+        var data=[];
+           
 
-    title: {
-        text: 'Solar Employment Growth by Sector, 2010-2016'
-    },
+    $http.get(API).then(function(response){
+        countries = response.data.map(function(d) { return d.country });
+        years = response.data.map(function(d) { return d.year });
+        names = response.data.map(function(d) { return d.name });
+        scorergoals = response.data.map(function(d) { return d.scorergoal });
+        scorermatches = response.data.map(function(d) { return d.scorermatch });
+        scoreraverages = response.data.map(function(d) { return d.scoreraverage });
+        
+        data= response.data;
+        console.log(names[1]);
 
-    subtitle: {
-        text: 'Source: thesolarfoundation.com'
-    },
+        countryandyear = response.data.map(function(d) { return d.country + " " + d.year });
+        sumas = response.data.map(function(d) { return d.scorergoal + d.scorermatch + d.scoreraverage });
 
-    yAxis: {
-        title: {
-            text: 'Number of Employees'
-        }
-    },
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
-    },
-
-    plotOptions: {
-        series: {
-            label: {
-                connectorAllowed: false
+        CandProperties = countryandyear.map(function(n, i) {
+            return {
+                name: countryandyear[i],
+                data: [scorergoals[i], scorermatches[i], scoreraverages[i]]
+            };
+        });
+        CandYsumas = countryandyear.map(function(n, i) {
+            return [n, sumas[i]];
+        });
+        
+        CandYsumas.unshift(['Country', 'Unemployments']);
+        
+        
+        
+        //Highcharts Basic Columnpie
+        Highcharts.chart('container', {
+            chart: {
+                type: 'column'
             },
-            pointStart: 2010
-        }
-    },
-
-    series: [{
-        name: 'Installation',
-        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-    }, {
-        name: 'Manufacturing',
-        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-    }, {
-        name: 'Sales & Distribution',
-        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-    }, {
-        name: 'Project Development',
-        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-    }, {
-        name: 'Other',
-        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-    }],
-
-    responsive: {
-        rules: [{
-            condition: {
-                maxWidth: 500
+            title: {
+                text: 'total Scorers by country and year'
             },
-            chartOptions: {
-                legend: {
-                    layout: 'horizontal',
-                    align: 'center',
-                    verticalAlign: 'bottom'
+            subtitle: {
+                text: 'in ES'
+            },
+            xAxis: {
+                categories: [
+                    "Goles", "Partidos", "Relacion"
+                ],
+                crosshair: true
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'number of jugadores'
                 }
+            },
+    labels: {
+        items: [{
+            html: 'Total fruit consumption',
+            style: {
+                left: '50px',
+                top: '18px',
+                color: (Highcharts.theme && Highcharts.theme.textColor) || 'black'
             }
         }]
-    }
+    },
+    series: [{
+        type: 'column',
+        name: names[0],
+        data: [scorergoals[0],scorermatches[0],scoreraverages[0]]
+    }, {
+        type: 'column',
+        name: 'John',
+        data: [2, 3, 5, 7, 6]
+    }, {
+        type: 'column',
+        name: 'Joe',
+        data: [4, 3, 3, 9, 0]
+    }, {
+        type: 'spline',
+        name: 'Average',
+        data: [3, 2.67, 3, 6.33, 3.33],
+        marker: {
+            lineWidth: 2,
+            lineColor: Highcharts.getOptions().colors[3],
+            fillColor: 'white'
+        }
+    }, {
+        type: 'pie',
+        name: 'Total consumption',
+        data: [{
+            name: "alw",
+            y: 13,
+            color: Highcharts.getOptions().colors[0] // Jane's color
+        }, {
+            name: 'piterlangi',
+            y: 23,
+            color: Highcharts.getOptions().colors[1] // John's color
+        }, {
+            name: 'Joe',
+            y: 19,
+            color: Highcharts.getOptions().colors[2] // Joe's color
+        }],
+        center: [100, 80],
+        size: 100,
+        showInLegend: false,
+        dataLabels: {
+            enabled: false
+        }
+    }]
+});
 
-});                
+                   
                     
                     
                     
+                    
+    });
 }]);
-
