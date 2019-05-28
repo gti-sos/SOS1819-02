abc4bc5b71784bb6aaae0cb3e6a48158e9985536
sos1819pgm <pabgarmar95@hotmail.com>
2019-05-28 18:45:13 +0000

    D03

--- public/api-companies/companiesGraphCtrl.js
+++ public/api-companies/companiesGraphCtrl.js
@@ -33,13 +33,11 @@ app.controller("grafComCtrl", ["$scope", "$http", "$routeParams", "$location", f
                 type: 'bar'
             },
             title: {
-                text: 'Historic World Population by Region'
+                text: ''
             },
-            subtitle: {
-                text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
-            },
+      
             xAxis: {
-                categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
+                categories: countries,
                 title: {
                     text: null
                 }
@@ -47,7 +45,7 @@ app.controller("grafComCtrl", ["$scope", "$http", "$routeParams", "$location", f
             yAxis: {
                 min: 0,
                 title: {
-                    text: 'Population (millions)',
+                    text: 'Income ',
                     align: 'high'
                 },
                 labels: {
@@ -80,16 +78,22 @@ app.controller("grafComCtrl", ["$scope", "$http", "$routeParams", "$location", f
             },
             series: [{
                 name: 'Year 1800',
-                data: [107, 31, 635, 203, 2]
+                data: [107, 31, 635, 203, 2, 3, 5, 7]
             }, {
                 name: 'Year 1900',
-                data: [133, 156, 947, 408, 6]
+                data: [133, 156, 947, 408, 6, 3, 5, 7]
             }, {
                 name: 'Year 2000',
-                data: [814, 841, 3714, 727, 31]
+                data: [814, 841, 3714, 727, 31, 3, 5, 7]
             }, {
                 name: 'Year 2016',
-                data: [1216, 1001, 4436, 738, 40]
+                data: [1216, 1001, 4436, 738, 40,  3, 5, 7]
+            }, {
+                name: 'Year 2012',
+                data: [1216, 1001, 4436, 738, 40,  3, 5, 7]
+            }, {
+                name: 'Year 2013',
+                data: [1216, 1001, 4436, 738, 40, 3, 5, 7, 55, 213, 2354]
             }]
         });
 
