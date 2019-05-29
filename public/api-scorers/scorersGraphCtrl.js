c7f442ad4aa39ddbe381af8b812f73efb714fcef
sos1819apc <albpercar@gmail.com>
2019-05-29 09:44:23 +0000

    a

--- public/api-scorers/scorersGraphCtrl.js
+++ public/api-scorers/scorersGraphCtrl.js
@@ -4,20 +4,16 @@ var app = angular.module("AppManager");
 app.controller("grafCtrl", ["$scope","$http","$routeParams", "$location", function ($scope, $http,$routeParams,$location){
                     console.log("Scorers editCtrl Initialized!");
                     var API = "/api/v1/scorers-stats";
+                    
     var countries = [];
     var years = [];
     var names = [];
     var scorergoals = [];
     var scorermatches = [];
     var scoreraverages = [];
-    var sumas = [];
-    var CandYsumas = [];
-    var countryandyear = [];
-    var CandProperties = [];
+   
     
-        var data=[];
-        var j=0;
-           
+          
 
     $http.get(API).then(function(response){
         countries = response.data.map(function(d) { return d.country });
@@ -27,119 +23,47 @@ app.controller("grafCtrl", ["$scope","$http","$routeParams", "$location", functi
         scorermatches = response.data.map(function(d) { return d.scorermatch });
         scoreraverages = response.data.map(function(d) { return d.scoreraverage });
         
-        data= response.data;
-        console.log(names[1]);
-
-        countryandyear = response.data.map(function(d) { return d.country + " " + d.year });
-        sumas = response.data.map(function(d) { return d.scorergoal + d.scorermatch + d.scoreraverage });
-
-        CandProperties = countryandyear.map(function(n, i) {
-            return {
-                name: countryandyear[i],
-                data: [scorergoals[i], scorermatches[i], scoreraverages[i]]
-            };
-        });
-        CandYsumas = countryandyear.map(function(n, i) {
-            return [n, sumas[i]];
-        });
-        
-        CandYsumas.unshift(['Country', 'Unemployments']);
-        
-        
-        
-        //Highcharts Basic Columnpie
-        
-        Highcharts.chart('container', {
-            chart: {
-                type: 'column'
-            },
-            title: {
-                text: 'total Scorers by country and year'
-            },
-            subtitle: {
-                text: 'in ES'
-            },
-            xAxis: {
-                categories: [
-                    "Goles", "Partidos", "Relacion"
-                ],
-                crosshair: true
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'number of jugadores'
-                }
-            },
-    labels: {
-        items: [{
-            html: 'Total goles y partidos',
-            style: {
-                left: '50px',
-                top: '18px',
-                color: (Highcharts.theme && Highcharts.theme.textColor) || 'black'
-            }
-        }]
+ Highcharts.chart('container', {
+    chart: {
+        type: 'line'
     },
-    series:[
-        {
-        type: 'column',
-        name: names[j],
-        data: [scorergoals[j],scorermatches[j],scoreraverages[j]],
-        operation: j=j+1
-        ,
-    }, {
-        type: 'column',
-        name: names[j],
-        data: [scorergoals[j],scorermatches[j],scoreraverages[j]],
-        operation: j=j+1,
-    }, {
-        type: 'column',
-        name: names[j],
-        data: [scorergoals[j],scorermatches[j],scoreraverages[j]],
-        operation: j=j+1,
-    }, {
-        type: 'spline',
-        name: 'Media',
-        operation: j=0,
-        data: [(scorergoals[j]+scorergoals[j+1]+scorergoals[j+2])/3,
-        (scorermatches[j]+scorermatches[j+1]+scorermatches[j+2])/3,
-        (scoreraverages[j]+scoreraverages[j+1]+scoreraverages[j+2])/3],
-    },{
-        marker: {
-            lineWidth: 2,
-            lineColor: Highcharts.getOptions().colors[3],
-            fillColor: 'white'
+    title: {
+        text: 'Highcharts'
+    },
+    subtitle: {
+        text: 'Scorers'
+    },
+    xAxis: {
+        text: 'jugadores',
+        categories: names
+    },
+    yAxis: {
+        title: {
+            text: 'Cantidad'
         }
-    }, {
-        type: 'pie',
-        operation: j=0,
-        name: 'Total consumption',
-        data: [{
-            name: names[j],
-            y: scorergoals[j]+scorermatches[j],
-            color: Highcharts.getOptions().colors[0], // Jane's color
-            operation: j=j+1,
-        }, {
-            name: names[j],
-            y: scorergoals[j]+scorermatches[j],
-            color: Highcharts.getOptions().colors[1], // John's color
-            operation: j=j+1,
-        }, {
-            name: names[j],
-            y: scorergoals[j]+scorermatches[j],
-            color: Highcharts.getOptions().colors[2], // Joe's color
-            operation: j=j+1,
-        }],
-        center: [100, 80],
-        size: 100,
-        showInLegend: false,
-        dataLabels: {
-            enabled: false
+    },
+    plotOptions: {
+        line: {
+            dataLabels: {
+                enabled: true
+            },
+            enableMouseTracking: false
         }
+    },
+    series: [{
+        name: 'goles',
+        data: scorergoals
+    }, {
+        name: 'Partidos jugados',
+        data: scorermatches
+    }, {
+        name: 'Promedio',
+        data: scoreraverages
     }]
 });
-
+        
+// Geo Chart
+      
 
                    
                     
