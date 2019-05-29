fbda4d9398b75bc6661ee2374a7e0295f37e5d7c
sos1819pgm <pabgarmar95@hotmail.com>
2019-05-29 09:54:20 +0000

    D03

--- public/api-companies/companiesGraphCtrl.js
+++ public/api-companies/companiesGraphCtrl.js
@@ -1,106 +1,89 @@
  /*global angular */
-  /*global Highcharts */
+ /*global Highcharts */
+ /*global newValue */
+ /*global newValue2 */
 
+
+ var app = angular.module("AppManager");
+
+ app.controller("grafComCtrl", ["$scope", "$http", "$routeParams", "$location", function($scope, $http, $routeParams, $location) {
+     console.log("Companies grafica ctrl Initialized!");
+     var API = "/api/v1/companies-stats";
+
+
+
+     $http.get(API).then(function(response) {
+
+         var data = [];
+
+         var countries = response.data.map(function(d) { return d.country });
+         var years = response.data.map(function(d) { return d.year });
+         var incomes = response.data.map(function(d) { return d.income });
+
+
+         var chardata2 = response.data.map(function(company) {
+             newValue = company.company;
+             newValue2 = company.income;
+
+             return [newValue, newValue2];
+
+         });
+
+
+
+         Highcharts.chart('container', {
+             chart: {
+                 type: 'column'
+             },
+             title: {
+                 text: 'Income in the 21st century:'
+             },
+             subtitle: {
+                 text: 'Source: <a href="https://es.wikipedia.org/wiki/Anexo:Empresas_por_ingresos">Wikipedia</a>'
+             },
+             xAxis: {
+                 type: 'companies',
+                 title: {
+                     text: 'Compañías'
+                 },
+
+                 labels: {
+                     rotation: -45,
+                     style: {
+                         fontSize: '13px',
+                         fontFamily: 'Verdana, sans-serif'
+                     }
+                 }
+             },
+             yAxis: {
+                 min: 0,
+                 title: {
+                     text: 'Income (millions)'
+                 }
+             },
+             legend: {
+                 enabled: false
+             },
+             tooltip: {
+                 pointFormat: 'Income: <b>{point.y:.1f} millions</b>'
+             },
+             series: [{
+                 name: 'Income',
+                 data: chardata2,
+                 dataLabels: {
+                     enabled: true,
+                     rotation: -90,
+                     color: 'orange',
+                     align: 'right',
+                     format: '{point.y:.1f}', // one decimal
+                     y: 10, // 10 pixels down from the top
+                     style: {
+                         fontSize: '13px',
+                         fontFamily: 'Verdana, sans-serif'
+                     }
+                 }
+             }]
+         });
+     });
+ }]);
  
-var app = angular.module("AppManager");
-
-app.controller("grafComCtrl", ["$scope", "$http", "$routeParams", "$location", function($scope, $http, $routeParams, $location) {
-    console.log("Companies grafica ctrl Initialized!");
-    var API = "/api/v1/companies-stats";
-    var companies = [];
-    var countries = [];
-    var years = [];
-    var incomes = [];
-    var marketcapitalizations = [];
-    var employees = [];
-
-
-    var data = [];
-    $http.get(API).then(function(response) {
-        countries = response.data.map(function(d) { return d.country });
-        years = response.data.map(function(d) { return d.year });
-        companies = response.data.map(function(d) { return d.company });
-        incomes = response.data.map(function(d) { return d.income });
-        marketcapitalizations = response.data.map(function(d) { return d.marketcapitalization });
-        employees = response.data.map(function(d) { return d.employee });
-
-        data = response.data;
-        console.log(companies[1]);
-
-
-        Highcharts.chart('container', {
-            chart: {
-                type: 'bar'
-            },
-            title: {
-                text: ''
-            },
-      
-            xAxis: {
-                categories: countries,
-                title: {
-                    text: null
-                }
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'Income ',
-                    align: 'high'
-                },
-                labels: {
-                    overflow: 'justify'
-                }
-            },
-            tooltip: {
-                valueSuffix: ' millions'
-            },
-            plotOptions: {
-                bar: {
-                    dataLabels: {
-                        enabled: true
-                    }
-                }
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'top',
-                x: -40,
-                y: 80,
-                floating: true,
-                borderWidth: 1,
-                backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-                shadow: true
-            },
-            credits: {
-                enabled: false
-            },
-            series: [{
-                name: 'Year 1800',
-                data: [107, 31, 63, 203, 2, 3, 5, 7]
-            }, {
-                name: 'Year 1900',
-                data: [133, 156, 97, 408, 6, 3, 5, 7]
-            }, {
-                name: 'Year 2000',
-                data: [84, 841, 14, 727, 31, 3, 5, 7]
-            }, {
-                name: 'Year 2016',
-                data: [126, 101, 100, 738, 40,  3, 5, 7]
-            }, {
-                name: 'Year 2012',
-                data: [116, 101, 436, 738, 40,  3, 5, 7]
-            }, {
-                name: 'Year 2013',
-                data: [116, 101, 55, 738, 40, 3, 5, 7, 55, 213, 254]
-            }]
-        });
-
-
-
-
-
-
-    });
-}]);
