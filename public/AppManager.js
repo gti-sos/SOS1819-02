4576230fe06bc9c2704f90efbe1184754896343d
sos1819apc <albpercar@gmail.com>
2019-05-30 11:55:54 +0000

    a

--- public/AppManager.js
+++ public/AppManager.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-        
+
         /*global angular*/
         
         angular.module("AppManager",["ngRoute"])
@@ -33,7 +32,7 @@
               templateUrl:"./api-scorers/scorersGraph.html",
               controller: "grafCtrl"
              })
-             .when("/integrations/api-scorers/api1",{
+             .when("/integrations/api1",{
               templateUrl:"./api-scorers/integracionAPI1.html",
               controller: "suicidesCtrl"
              })
@@ -52,61 +51,14 @@
              .when("/analytics",{
               templateUrl:"./analytics.html",
              
-             });
-            });
-=======
-        angular.module("AppManager", ["ngRoute"])
-         .config(function($routeProvider) {
-          $routeProvider
-           .when("/", {
-            templateUrl: "/principal.html",
-
-           })
-           .when("/api-movies/", {
-            templateUrl: "./api-movies/list.html",
-            controller: "ListCtrlMovies"
-           })
-           .when("/api-movies/:country/:year", {
-            templateUrl: "./api-movies/edit.html",
-            controller: "editCtrlMovies"
-           })
-           .when("/analytics/api-movies/", {
-            templateUrl: "./api-movies/moviesGraph.html",
-            controller: "grafMoviesCtrl"
-           })
-           .when("/api-scorers/", {
-            templateUrl: "./api-scorers/list.html",
-            controller: "ListCtrl"
-           })
-           .when("/api-scorers/:country/:year", {
-            templateUrl: "./api-scorers/edit.html",
-            controller: "editCtrl"
-           })
-           .when("/api-scorers/prueba", {
-            templateUrl: "./api-scorers/scorersGraph.html",
-            controller: "grafCtrl"
-           })
-           .when("/api-companies/", {
-            templateUrl: "./api-companies/list.html",
-            controller: "ListCtrlcompanies"
-           })
-           .when("/api-companies/:country/:year", {
-            templateUrl: "./api-companies/edit.html",
-            controller: "editCtrlcompanies"
-           })
-           .when("/analytics/api-companies", {
-            templateUrl: "./api-companies/companiesGraph.html",
-            controller: "grafComCtrl"
-           })
-           .when("/analytics", {
-            templateUrl: "./analytics.html",
-           })
-           .when("/integrations", {
+             })
+             .when("/integrations", {
             templateUrl: "./integrations.html",
-           })
+            })
            .when("/about", {
             templateUrl: "./about.html",
 
            });
-         });
->>>>>>> bf29657bef09b9bb094b7419ccd0cc4851bec421
+            });
+
+
