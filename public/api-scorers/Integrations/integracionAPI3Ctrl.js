05f3cccfd5cdf595f7cab424b0f3b332d80f5219
sos1819apc <albpercar@gmail.com>
2019-05-31 18:11:00 +0000

    a

---
+++ public/api-scorers/Integrations/integracionAPI3Ctrl.js
@@ -0,0 +1,26 @@
+/* global angular */
+
+var app = angular.module("AppManager");
+
+app.controller("excountryCtrl", ["$scope", "$http", function($scope, $http) {
+
+
+    console.log("List Suicides Controller initialized.");
+
+    var API = "https://sos1819-08.herokuapp.com/api/v1/expenses-of-countries-in-education-and-culture";
+    refresh();
+
+    function refresh() {
+
+        console.log("Requesting to <" + API + ">...");
+        $http
+            .get(API)
+            .then(function(response) {
+
+                console.log("Data received:" + JSON.stringify(response.data, null, 2));
+
+                $scope.companies = response.data;
+            });
+    }
+
+}]);
