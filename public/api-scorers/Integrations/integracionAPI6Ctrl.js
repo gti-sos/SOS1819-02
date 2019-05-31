3df2552b29303a0260a8b671304b36d571526929
sos1819apc <albpercar@gmail.com>
2019-05-31 18:50:57 +0000

    a

---
+++ public/api-scorers/Integrations/integracionAPI6Ctrl.js
@@ -0,0 +1,26 @@
+/* global angular */
+
+var app = angular.module("AppManager");
+
+app.controller("unemploymentCtrl", ["$scope", "$http", function($scope, $http) {
+
+
+    console.log("List Suicides Controller initialized.");
+
+    var API = "https://sos1819-12.herokuapp.com/api/v1/youth-unemployment-stats";
+    refresh();
+
+    function refresh() {
+
+        console.log("Requesting to <" + API + ">...");
+        $http
+            .get(API)
+            .then(function(response) {
+
+                console.log("Data received:" + JSON.stringify(response.data, null, 2));
+
+                $scope.unemployments= response.data;
+            });
+    }
+
+}]);
