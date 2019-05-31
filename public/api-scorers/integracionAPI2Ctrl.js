a02406e070deffe6f52685ffa73f40f98d91b442
sos1819apc <albpercar@gmail.com>
2019-05-31 17:42:27 +0000

    a

---
+++ public/api-scorers/integracionAPI2Ctrl.js
@@ -0,0 +1,26 @@
+/* global angular */
+
+var app = angular.module("AppManager");
+
+app.controller("compaCtrl", ["$scope", "$http", function($scope, $http) {
+
+
+    console.log("List Suicides Controller initialized.");
+
+    var API = "https://sos1819-03.herokuapp.com/api/v1/companies";
+    refresh();
+
+    function refresh() {
+
+        console.log("Requesting biofuels to <" + API + ">...");
+        $http
+            .get(API)
+            .then(function(response) {
+
+                console.log("Data received:" + JSON.stringify(response.data, null, 2));
+
+                $scope.companies = response.data;
+            });
+    }
+
+}]);
