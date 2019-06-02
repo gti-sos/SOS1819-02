6b2e002bbdd156306f81f9da208268e80c3eae54
sos1819apc <albpercar@gmail.com>
2019-06-02 14:39:42 +0000

    a

--- public/api-scorers/Integrations/integracionAPI10Ctrl.js
+++ public/api-scorers/Integrations/integracionAPI10Ctrl.js
@@ -17,10 +17,58 @@ app.controller("extCtrl", ["$scope", "$http", function($scope, $http) {
             .get(API)
             .then(function(response) {
 
-                console.log("Data received:" + JSON.stringify(response.data, null, 2));
+            //    console.log("Data received:" + JSON.stringify(response.data, null, 2));
+        var unionEuropea = [];
+        var nombres = [];
+        var population = [];
+        nombres = response.data.map(function(d) { return d.name });
+        population = response.data.map(function(d) { return d.population });
+        console.log("esto son los nombres" +population);
 
-                $scope.exts= response.data;
-            });
+        var responseData = (response.data);
+        console.log(responseData)
+
+    var res = [];
+        
+        for (var i=0; i< nombres.length;i++){
+     res.push([nombres[i], population[i]]);
+   }
+        console.log("res------------->" + res)
+        
+
+              //  $scope.exts= response.data;
+           
+        Highcharts.chart('container', {
+    chart: {
+        type: 'funnel'
+    },
+    title: {
+        text: 'popularidad de los paises',
+        x: -50
+    },
+    plotOptions: {
+        series: {
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b> ({point.y:,.0f})',
+                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
+                softConnector: true
+            },
+            center: ['40%', '50%'],
+            width: '80%'
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    series: [{
+        name: 'Unique users',
+        data:
+            res
+        
+    }]
+        });
+});
     }
 
 }]);
