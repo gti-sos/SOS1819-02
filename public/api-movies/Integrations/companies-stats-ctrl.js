3cab4e13091742b4e2ab28ecf10b9ed2a9c95169
sos1819amm <alemarman1@gmail.com>
2019-06-03 15:02:15 +0000

    aa

--- public/api-movies/Integrations/companies-stats-ctrl.js
+++ public/api-movies/Integrations/companies-stats-ctrl.js
@@ -1,26 +1,92 @@
-/* global angular */
+angular
+    .module("AppManager")
+    .controller("companiesCtrl", ["$scope", "$http", function($scope, $http) { //$scope permite acceder a los datos, al modelo.
+        //$http establece la conexión entre el navegador del usuario y el servidor (backend).
 
-var app = angular.module("AppManager");
+        console.log("Companies Controller initialized");
 
-app.controller("companiesCtrl", ["$scope", "$http", function($scope, $http) {
+        var apiCompanies = "https://sos1819-02.herokuapp.com/api/v1/companies-stats";
+        var apiMovies = "https://sos1819-02.herokuapp.com/api/v1/movies-stats";
 
 
-    console.log("List Companies Controller initialized.");
+        $http.get(apiCompanies).then(function(response1) {
+                $http.get(apiMovies).then(function(response2) {
+                   
 
-    var API = "https://sos1819-02.herokuapp.com/api/v1/companies-stats/";
-    refresh();
+                   
+           console.log("Data Companies received:" + JSON.stringify(response1.data, null, 2));
+            console.log("Data Movies received:" + JSON.stringify(response2.data, null, 2));
 
-    function refresh() {
 
-        console.log("Requesting movies to <" + API + ">...");
-        $http
-            .get(API)
-            .then(function(response) {
+var aux1 = []
+var aux2 = []
 
-                console.log("Data received:" + JSON.stringify(response.data, null, 2));
 
-                $scope.companies = response.data;
-            });
+var res1 = response1.data
+
+var res2 = response2.data
+
+for (var i=0; i< res2.length;i++){
+     aux1.push(res1[i].country);
+   }
+   for (var i=0; i< res2.length;i++){
+     aux2.push(res2[i].movieedition);
+   }
+
+
+
+
+//console.log("aux :"+ aux);
+
+
+                   
+///////////////////////////////////   CHARTS      ///////////////////////////////
+
+
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: aux1,
+        datasets: [{
+            label: 'number of edition',
+            data: aux2,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
     }
+});
 
-}]);
+                   
+                   
+                });
+
+        });
+
+
+    }]);
+
