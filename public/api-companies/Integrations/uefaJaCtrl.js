116f0fdd1899c5c72bfc76a2a0b1a2d4c27c6729
sos1819pgm <pabgarmar95@hotmail.com>
2019-06-04 13:06:35 +0000

    D03

--- public/api-companies/Integrations/uefaJaCtrl.js
+++ public/api-companies/Integrations/uefaJaCtrl.js
@@ -22,5 +22,67 @@ app.controller("uefaJaCtrl", ["$scope", "$http", function($scope, $http) {
                 $scope.uefaclubrankings = response.data;
             });
     }
+    var apiPropia = "/api/v1/companies-stats";
+    var api = "https://sos1819-06.herokuapp.com/api/v1/uefa-club-rankings/";
 
+
+
+    $http.get(api).then(function(response1) {
+        $http.get(apiPropia).then(function(response2) {
+
+    var years = [];
+
+    var data=[];
+
+
+            var countries = response2.data.map(function(d) { return d.country });
+            var years = response2.data.map(function(d) { return d.year });
+            var incomes = response2.data.map(function(d) { return d.income });
+
+
+            var chart = am4core.create("chartdiv", am4charts.RadarChart);
+            // Themes begin
+            am4core.useTheme(am4themes_animated);
+            // Themes end
+
+            // Create chart instance
+            var chart = am4core.create("chartdiv", am4charts.XYChart3D);
+
+           
+            chart.data = [{
+                "year": data[0].year,
+                "country": data[0].country,
+                "color": chart.colors.next()
+            }, {
+                "year": data[5].year,
+                "country": data[5].country,
+                "color": chart.colors.next()
+            }, {
+                "year": data[6].year,
+                "country": data[6].country,
+                "color": chart.colors.next()
+            }];
+            console.log("auxAmChart:");
+            //        console.log(JSON.stringify(auxAmChart,null,2));
+
+            // Create axes
+            var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+            categoryAxis.dataFields.category = "year";
+            categoryAxis.numberFormatter.numberFormat = "#";
+            categoryAxis.renderer.inversed = true;
+
+            var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+
+            // Create series
+            var series = chart.series.push(new am4charts.ColumnSeries3D());
+            series.dataFields.valueX = "country";
+            series.dataFields.categoryY = "year";
+            series.name = "country";
+            series.columns.template.propertyFields.fill = "color";
+            series.columns.template.tooltipText = "{valueX}";
+            series.columns.template.column3D.stroke = am4core.color("#fff");
+            series.columns.template.column3D.strokeOpacity = 0.2;
+
+        });
+    });
 }]);
