bf6867d65fc2da822aad9275e4478f301cd690a0
sos1819pgm <pabgarmar95@hotmail.com>
2019-06-04 18:06:50 +0000

    D03

--- public/api-companies/Integrations/educationCtrl.js
+++ public/api-companies/Integrations/educationCtrl.js
@@ -1,26 +1,62 @@
 /* global angular */
 
 var app = angular.module("AppManager");
+var datos = []
 
 app.controller("educationCtrl", ["$scope", "$http", function($scope, $http) {
 
 
-    console.log("List  Controller initialized.");
 
-    var API = "https://sos1819-11.herokuapp.com/api/v2/public-expenditure-educations";
-    refresh();
+        $http.get("https://sos1819-02.herokuapp.com/api/v1/companies-stats").then(function(response) {
 
-    function refresh() {
+            for (var i = 0; i < response.data.length; i++) {
 
-        console.log("Requesting  to <" + API + ">...");
-        $http
-            .get(API)
-            .then(function(response) {
+                datos.push({ x: response.data[i].marketcapitalization * 1000, y: response.data[i].income * 1000, z: response.data[i].year, name: response.data[i].country });
 
-                console.log("Data received:" + JSON.stringify(response.data, null, 2));
+            }
 
-                $scope.generalPublicExpenses = response.data;
+            $http.get("https://sos1819-11.herokuapp.com/api/v2/public-expenditure-educations").then(function(response) {
+
+                for (var i = 0; i < response.data.length; i++) {
+                    datos.push({ x: response.data[i].educationExpense, y: response.data[i].educationExpense, z: response.data[i].year, name: response.data[i].country });
+
+                }
+
+
+
+                var chart = new CanvasJS.Chart("chartContainer", {
+                    animationEnabled: true,
+                    zoomEnabled: true,
+                    theme: "light2",
+                    title: {
+                        text: "Capitalization And Education Expense"
+                    },
+                    axisX: {
+                        title: "€",
+                        suffix: "€",
+                        minimum: 0,
+                        maximum: 200000,
+                        gridThickness: 1
+                    },
+                    axisY: {
+                        title: "",
+                        suffix: ""
+                    },
+                    data: [{
+                        type: "bubble",
+                        toolTipContent: "<b>{name}</b><br/>Expense: {x} m <br/> Income: {y} m. <br/> Year: {z}",
+                        dataPoints: datos
+                    }]
+                });
+
+                chart.render();
+
+
+
             });
+
+
+        });;
     }
 
-}]);
+]);
--- public/api-companies/Integrations/muertoCtrl.js
+++ public/api-companies/Integrations/muertoCtrl.js
@@ -6,7 +6,7 @@ app.controller("muertoCtrl", ["$scope", "$http", function($scope, $http) {
 
 
     console.log("List  Controller initialized.");
- var API2 = "/api/v1/companies-stats";
+    var API2 = "/api/v1/companies-stats";
     var API = "https://sos1819-14.herokuapp.com/api/v1/deceaseds/";
     refresh();
 
@@ -34,35 +34,7 @@ app.controller("muertoCtrl", ["$scope", "$http", function($scope, $http) {
                     console.log("Data received:" + JSON.stringify(response.data, null, 2));
 
 
-                    var aux = response.data.map(function(d) { return d.life });
-                    var aux2 = response1.data.map(function(d) { return d.income });
-                    var res = [];
-                    var i = 0;
-                    var j = 10;
-                    while (i < 6) {
-                        res.push({ x: j, y: aux2[i], indexLabel: "income" }, { x: j, y: aux[i], indexLabel: "life" });
-                        j = j + 10;
-                        i++;
-                    }
-
-
-                    var chart = new CanvasJS.Chart("chartContainer", {
-                        animationEnabled: true,
-                        exportEnabled: true,
-                        theme: "light1", // "light1", "light2", "dark1", "dark2"
-                        title: {
-                            text: "CanvasJS:doughnut"
-                        },
-                        data: [{
-                            type: "doughnut", //change type to bar, line, area, pie, etc
-                            //indexLabel: "{y}", //Shows y value on all Data Points
-                            indexLabelFontColor: "#5A5757",
-                            indexLabelPlacement: "outside",
-                            dataPoints: res
-
-                        }]
-                    });
-                    chart.render();
+                    ;
                 });
         });
 }]);
