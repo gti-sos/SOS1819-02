2611cc738377517b8a39559cb3c834d854fa9335
sos1819apc <albpercar@gmail.com>
2019-06-04 15:30:01 +0000

    a

--- public/api-scorers/Integrations/integracionAPI4Ctrl.js
+++ public/api-scorers/Integrations/integracionAPI4Ctrl.js
@@ -23,31 +23,85 @@ app.controller("peliculasCtrl", ["$scope", "$http", function($scope, $http) {
             .then(function(response1) {
 
                 console.log("Data received:" + JSON.stringify(response.data, null, 2));
-                var countries1 = [];
-                var countries2 = [];
+               
                 
-                countries1 = response.data.map(function(d) { return d.country });
-                countries2 = response1.data.map(function(d) { return d.country });
+                var year1 = response.data.map(function(d) { return d.year });
+                var year2 = response1.data.map(function(d) { return d.year });
                 var edition = response.data.map(function(d) { return d.movieedition });
                 var goles = response1.data.map(function(d) { return d.scorergoal });
                 var res = []
-                for (var i=0; i< goles.length;i++){
-                res.push({ year: "200"+i, value: edition[i] },{ year: "200"+i, value: goles[i] });
-                 }
-                 new Morris.Line({
-  // ID of the element in which to draw the chart.
-  element: 'myfirstchart',
-  // Chart data records -- each entry in this array corresponds to a point on
-  // the chart.
-  data: res,
-  
-  // The name of the data record attribute that contains x-values.
-  xkey: 'year',
-  // A list of names of data record attributes that contain y-values.
-  ykeys: ['value'],
-  // Labels for the ykeys -- will be displayed when you hover over the
-  // chart.
-  labels: ['Value']
+               var i =0;
+                
+//                 while(i<year2.length){
+//                     res.push({
+//   "year": year1[i],
+//   "income": edition[i],
+//   "color": chart.colors.next()
+// });
+//                     i++;
+//                 }
+                am4core.ready(function() {
+
+// Themes begin
+am4core.useTheme(am4themes_animated);
+// Themes end
+
+// Create chart instance
+var chart = am4core.create("chartdiv", am4charts.XYChart3D);
+
+// Add data
+chart.data =[{
+  "year": year1[0],
+  "income": edition[0],
+  "color": chart.colors.next()
+},{
+  "year": year1[1],
+  "income": edition[1],
+  "color": chart.colors.next()
+},{
+  "year": year1[2],
+  "income": edition[2],
+  "color": chart.colors.next()
+},{
+  "year": year1[3],
+  "income": edition[3],
+  "color": chart.colors.next()
+},{
+  "year": year2[0],
+  "income": goles[0],
+  "color": chart.colors.next()
+},{
+  "year": year2[1],
+  "income": goles[1],
+  "color": chart.colors.next()
+},{
+  "year": year2[2],
+  "income": goles[2],
+  "color": chart.colors.next()
+},{
+  "year": year2[3],
+  "income": goles[3],
+  "color": chart.colors.next()
+}];
+
+// Create axes
+var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+categoryAxis.dataFields.category = "year";
+categoryAxis.numberFormatter.numberFormat = "#";
+categoryAxis.renderer.inversed = true;
+
+var  valueAxis = chart.xAxes.push(new am4charts.ValueAxis()); 
+
+// Create series
+var series = chart.series.push(new am4charts.ColumnSeries3D());
+series.dataFields.valueX = "income";
+series.dataFields.categoryY = "year";
+series.name = "Income";
+series.columns.template.propertyFields.fill = "color";
+series.columns.template.tooltipText = "{valueX}";
+series.columns.template.column3D.stroke = am4core.color("#fff");
+series.columns.template.column3D.strokeOpacity = 0.2;
+
 });
 
             }); 
--- public/api-scorers/Integrations/integracionAPI8Ctrl.js
+++ public/api-scorers/Integrations/integracionAPI8Ctrl.js
@@ -8,6 +8,7 @@ app.controller("carCtrl", ["$scope", "$http", function($scope, $http) {
     console.log("Controller initialized.");
 
     var API = "https://sos1819-10.herokuapp.com/api/v1/e-car-statics";
+    var API2 = "/api/v1/scorers-stats";
     refresh();
 
     function refresh() {
@@ -16,11 +17,59 @@ app.controller("carCtrl", ["$scope", "$http", function($scope, $http) {
         $http
             .get(API)
             .then(function(response) {
+        console.log("Requesting to <" + API + ">...");
+        $http
+            .get(API2)
+            .then(function(response1) {
 
                 console.log("Data received:" + JSON.stringify(response.data, null, 2));
 
                 $scope.cars= response.data;
                 console.log("aaaa");
+                var year1 = response.data.map(function(d) { return d.year });
+                var country1 = response.data.map(function(d) { return d.country });
+                var year2 = response1.data.map(function(d) { return d.year });
+                var country2 = response1.data.map(function(d) { return d.country });
+                var promediogoles = response1.data.map(function(d) { return d.scoreraverage });
+                var disponibles = response1.data.map(function(d) { return d.marketPart });
+                
+                 var res = []
+                
+                var i =0;
+                
+                while(i<year2.length){
+                    res.push({
+    country: country1[i]+" "+year1[i],
+    value: disponibles[i]
+  },
+  {
+    country: country2[i]+" "+year2[i],
+    value: promediogoles[i]
+  });
+                    i++;
+                }
+                am4core.useTheme(am4themes_animated);
+// Themes end
+
+// Create chart
+var chart = am4core.create("chartdiv", am4charts.PieChart);
+chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
+
+chart.data = res;
+
+var series = chart.series.push(new am4charts.PieSeries());
+series.dataFields.value = "value";
+series.dataFields.radiusValue = "value";
+series.dataFields.category = "country";
+series.slices.template.cornerRadius = 6;
+series.colors.step = 3;
+
+series.hiddenState.properties.endAngle = -90;
+
+chart.legend = new am4charts.Legend();
+                
+                
+            });  
             });
     }
 
